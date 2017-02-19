9658a213373e1af5263269e0c0ae6e7c7729b01a
Barrett Allen <barrettallen@Barretts-MacBook.local>
2017-02-19 18:02:42 -0500

    Committing all working changes to game and js file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,64 @@
-//Defines an array for words to be guessed
+//Defines initial varialbles and sets to zero
 
-var wordBank = ["wicked", "waitress", "hamilton"];
-
-//Defines the letter bank to be used
-
-var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-//Defines variables for wins and losses
-
+var currentLetter;
+var userGuess = [""];
 var wins = 0;
 var losses = 0;
+var guessesLeft = 9;
 
+//Array of letters and keypressed var set
 
-//define variables for: Input, Remaining Letters, Guesses, Current Word, Reveal Word?
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var keyPressed;
-var remainingLetters;
-var guesses;
-var currentWord;
-var revealWord;
+//Onload with writing the html
 
-//ondocument ready function
+window.onload = function() {
+	
+	//New Game Function
 
-$(document).ready(function() {
+	function newGame () {
+		currentLetter = letters[Math.floor(Math.random() * letters.length)];
+		console.log("currentLetter: " + currentLetter);
+		guessesLeft = 9;
+		userGuess = [""];
+		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		document.querySelector("#losses").innerHTML = "Losses: " + losses;
+		document.querySelector("#guesses-left").innerHTML = "Guesses Left: " + guessesLeft;
+		document.querySelector("#guesses-so-far").innerHTML = "Your Guesses so far: ";
+	};
 
+	//Calling initial new game function
 
-//Starts the new game function
-
-	function newGame() {
-		currentWord = wordBank[Math.floor(Math.random() * wordBank.length)].toUpperCase();
-	}
 	newGame();
-	console.log(currentWord);
 
-//Choose a word at random and write the blank spaces to page
+	//When a key is pressed
 
-//Check if selected letter is part of current word
-
-});
+	document.onkeyup = function(event) {
+		if (event.keyCode >= 65 
+				&& event.keyCode <= 90 
+				&& event.key != userGuess[1] 
+				&& event.key != userGuess[2] 
+				&& event.key != userGuess[3] 
+				&& event.key != userGuess[4] 
+				&& event.key != userGuess[5] 
+				&& event.key != userGuess[6] 
+				&& event.key != userGuess[7] 
+				&& event.key != userGuess[8] 
+				&& event.key != userGuess[9]) {
+			userGuess.push(event.key);
+			if (event.key === currentLetter) {
+				wins++;
+				newGame();
+			} else {
+				guessesLeft--;
+				if (guessesLeft === 0) {
+					losses++;
+					newGame();
+				} else {
+				document.querySelector("#guesses-left").innerHTML = "Guesses Left: " + guessesLeft;
+				document.querySelector("#guesses-so-far").append(" " + event.key + ",");
+				}
+			}
+		};
+	};
+};
